26f29750d305809a9486bb49f686b695c8abbfa1
Robin Fussell <rfussell17@gmail.com>
2019-11-22 14:41:45 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+
+function todoList(){
+  //let todos = [];
+  const deleteButton;
+  const listItem;
+  const inputBox;
+
+
+  inputBox.addEventListener('click', () => {
+
+  }); 
+
+  listItem.addEventListener('click', () => {
+    
+  }); 
+
+  deleteButton.addEventListener('click', () => {
+    
+  }); 
+}
