e0cba4ef021d5656e5a53088e12c4c9aa2000889
Robin Fussell <rfussell17@gmail.com>
2019-11-25 09:02:25 -0800

    started js

--- script.js
+++ script.js
@@ -1,20 +1,26 @@
 
-function todoList(){
-  //let todos = [];
-  const deleteButton = document.getElementById("span");
-  const listItem = document.getElementById("list");
-  //const inputBox;
+  const input = document.getElementById("input");
+  let li = document.createElement("li")
+  let txtNode = document.createTextNode(input);
 
-
-  inputBox.addEventListener('click', () => {
-
-  }); 
-
-  listItem.addEventListener('click', () => {
-    console.log("clicked");
-  }); 
-
-  deleteButton.addEventListener('click', () => {
-    
-  }); 
+  function toDo(){
+  li.appendChild(txtNode);
+  list.appendChild(li)
+  }
+  
+  function handleEnter(e){
+    var keycode = (e.keyCode ? e.keyCode : e.which);
+    if (keycode == '13') {
+      alert('You pressed enter! - plain javascript');
+    }
 }
+
+  if (e.keyCode == 13) {
+      const toDo = input.value;
+      if(toDo){
+        addTodo(toDo);
+      }
+      input.value = "";
+     }
+     
+      
