a4d6ea83ca6d0ed75407c70b45e90dc38f88c815
Robin Fussell <rfussell17@gmail.com>
2019-11-27 16:42:16 -0800

    grey out tasks update

--- script.js
+++ script.js
@@ -1,9 +1,33 @@
 
-let button = document.getElementById("")
+let toDoList = ['bing', 'bong', 'chug', 'chugga'];
+let input = document.getElementById("input");
+let list = document.getElementById("ul");
+let li = document.getElementById("li") ;
+let deleteToDo = document.querySelector("X");
+
+for(let i = 0; i < toDoList.length; i++);
+//toDoList.forEach(toDo);
+//toDo.push(newTask);
+
+function toDo(){
+  li.appendChild(input);
+  ul.appendChild(li);
+  }
 
 
 
-/*
+function crossOffToDo(){
+  document.getElementById("li").style.color = "grey";
+  document.getElementById("li").style.textDecoration = "line-through";
+  
+    console.log("li clicked");
+}
+    checkToDo = document.getElementById('li'.toggleMe).addEventListener('click', () => {
+      crossOffToDo();
+    });
+
+
+    /*
   const input = document.getElementById("input");
   let li = document.createElement("li")
   let txtNode = document.createTextNode(input);
@@ -22,16 +46,3 @@ let button = document.getElementById("")
       input.value = "";
      }
      */
-
-
-function crossOffToDo(){
-
-  let checkToDo = document.getElementById('li');
-  checkToDo.style.color = "gray";
-  checkToDo.style.textDecorationLinethrough;
-  
-    console.log("li clicked");
-}
-    checkToDo = document.getElementById('li').addEventListener('click', () => {
-      crossOffToDo();
-    });
