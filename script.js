cc10180fcc3cdc001954296773d9da6d24909e9b
Robin Fussell <rfussell17@gmail.com>
2019-11-27 21:17:58 -0800

    check items off completed

--- script.js
+++ script.js
@@ -1,25 +1,76 @@
 
 let toDoList = [];
-let input = document.querySelector("input");
-let list = document.querySelector("ul");
-let listItem = document.querySelectorAll("li") ;
-let deleteToDo = document.querySelectorAll("X");
+const input = document.querySelector("input");
+const list = document.querySelector("ul");
+const listItem = document.querySelectorAll("li") ;
+let deleteToDo = document.querySelectorAll("trash");
+let check = document.getElementById("check");
+
+//input.addEventListener('keydown', runEvent);
+
+//const newListItem = document.createElement('li');
+//li.id = 'listItem';
+//li.setAttribute();
+
+//document.querySelector('ul').appendChild(li);
+
 
 //toDo.push(newTask);
 
-function toDo(){
-  li.appendChild(input);
-  ul.appendChild(li);
+function addToDo(e){
+  if(input.value === ''){
+    alert("You need to input something!");
   }
 
+  const li = document.createElement('li');
+  li.appendChild(document.createTextNode(input.value));
+  input.appendChild(li);
 
-function crossOffToDo(){
- document.getElementById('li').addEventListener('click', () => {
-  document.getElementById("li").style.color = "grey";
-  document.getElementById("li").style.textDecoration = "line-through";
-  console.log("li clicked");
-  });
-}
+  input.value ='';
+  }
+
+   //check.addEventListener('click', () => {
+    // console.log("check was clicked")
+  //  document.getElementById("li").style.color = "grey";
+  //  document.getElementById("li").style.textDecoration = "line-through";
+  // });
+
+  document.querySelector('li')[
+    document.addEventListener ? 'addEventListener' : 'attachEvent'
+]('click', function() {
+    this.style.color = this.style.color === 'black' ?
+        'grey' : 'black';
+        if(this.style.color === 'black'){
+          this.style.textDecoration = 'none'
+        } else {
+          this.style.textDecoration = 'line-through'
+        }
+});
+
+
+
+//function crossOffToDo(){
+ //document.getElementById('li').addEventListener('click', () => {
+ // document.getElementById("li").style.color = "grey";
+ // document.getElementById("li").style.textDecoration = "line-through";
+ // console.log("li clicked");
+ // });
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     /*
   const input = document.getElementById("input");
   let li = document.createElement("li")
