06ddff6b0177709dc4752b3faeb7e55195090f78
Robin Fussell <rfussell17@gmail.com>
2019-12-02 11:53:28 -0800

    scroll added to container

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//Add li's to ul via input box
 
 function addTask(){
   let input = document.getElementById("input").value;
@@ -7,16 +8,20 @@ function addTask(){
     document.getElementById("ul").appendChild(newTask);
     form.reset();
 
+//cross out completed to-dos    
+
   newTask.addEventListener('click', () => {
     newTask.style.textDecoration = newTask.style.textDecoration === 'none' ?
       'line-through' : 'none';
     }); 
 
+//clear all completed to-dos
+
   let clearBtn = document.getElementById("clear");
   clearBtn.addEventListener('click', () => {
     if(newTask.style.textDecoration === 'line-through')
     newTask.innerHTML = '';
- });
-};
+  });
+ };
 
 
