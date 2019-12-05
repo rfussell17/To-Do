d973a53d04a0b6b73ff20ce47b355fa50da2327d
Robin Fussell <rfussell17@gmail.com>
2019-12-04 22:41:29 -0800

    cleaned up code

--- script.js
+++ script.js
@@ -1,27 +1,36 @@
+//connect event listener to add button
+
+document.querySelector("#button").onclick = function(e) {
+  addTask();
+};
+
 //Add li's to ul via input box
 
-function addTask(){
+function addTask() {
   const input = document.getElementById("input").value;
   let task = document.createTextNode(input);
   let newTask = document.createElement("li");
-    newTask.appendChild(task);
-    document.getElementById("ul").appendChild(newTask);
-    form.reset();
+  newTask.appendChild(task);
+  document.getElementById("ul").appendChild(newTask);
+  form.reset();
 
-//cross out completed to-dos    
+  //cross out completed to-dos
 
-  newTask.addEventListener('click', () => {
-    newTask.style.textDecoration = newTask.style.textDecoration === 'none' ?
-      'line-through' : 'none';
-    }); 
-
-//clear all completed to-dos
-
-  const clearBtn = document.getElementById("clear");
-  clearBtn.addEventListener('click', () => {
-    if(newTask.style.textDecoration === 'line-through')
-    newTask.innerHTML = '';
+  newTask.addEventListener("click", () => {
+    newTask.style.textDecoration =
+      newTask.style.textDecoration === "line-through" ? "none" : "line-through";
   });
- };
+}
 
+  //clear all completed to-dos
 
+const clearBtn = document.getElementById("clear");
+clearBtn.addEventListener("click", () => {
+
+  const liElements = document.querySelectorAll("li");
+  liElements.forEach(li => {
+    if(li.style.textDecoration === "line-through"){
+      li.innerHTML = "";
+    }
+  });
+});
